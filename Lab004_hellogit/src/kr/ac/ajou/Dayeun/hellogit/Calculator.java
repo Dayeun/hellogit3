d3a4dfa1458dbb7b72598c3c35960a005cc45713
Dayeun <turtleneck@ajou.ac.kr>
2016-10-04 11:39:34 +0900

    Dayeun

---
+++ Lab004_hellogit/src/kr/ac/ajou/Dayeun/hellogit/Calculator.java
@@ -0,0 +1,34 @@
+package kr.ac.ajou.Dayeun.hellogit;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public double add(double x, double y){
+		return 0;
+	}
+	
+	public double subtract(double x, double y){
+		return 0;
+	}
+	
+	public double divide(double x, double y){
+		return 0;
+	}
+	
+	public double multiply(double x, double y){
+		return 0;
+	}
+	
+	public int remainder(double x, double y){
+		return 0;
+	}
+	public String toString(){
+		
+		return null;
+	}
+
+}
